e5b29b2feac565eaa04773e84cdfa54e2df07965
Nicholas Augusto <nicholas.augusto@cs.cruzeirodosul.edu.br>
2026-08-12 01:33:37 -0300

    Refinando função calculaBonus

--- src/main/kotlin/operadores.kt
+++ src/main/kotlin/operadores.kt
@@ -7,16 +7,30 @@ menos de um ano -> 500
 fun calculabonus(tempo: Float): Float {
     if (tempo in 0f..0.9f) {
         return 500f
-    }else if (tempo in 1f..3.9f) {
+    } else if (tempo in 1f..3.9f) {
         return 2000f
-    }else {
+    } else if (tempo >= 4){
         return 5000f
     }
-
+    return 0f
 }
-fun main(){
-    print("A quantos anos está na empresa(para meses insira dessa forma ex:0.6 = 6 meses) :")
-    val tempoEmpresa = readln().toFloat()
 
-    println("Seu bonus salarial, com base em tempo de empresa é de R$ ${calculabonus(tempoEmpresa)}")
+fun main() {
+    while (true) {
+        print("---------------Calculo de bonus salarial------------------ \n" +
+        "Para meses insira da seguinte forma ex:0.6 = 6 meses \n" +
+        "Para finalizar digite -1 \n"+
+        "Por favor, digite a quanto anos ou meses está na empresa : ")
+        val tempoEmpresa = readln().toFloat()
+
+        if(tempoEmpresa != -1f){
+            println("Seu bonus salarial, com base em tempo de empresa é de R$ ${calculabonus(tempoEmpresa)}")
+        }
+        if (tempoEmpresa == -1f) {
+            println("Você digitou : -1 Comando Sair")
+            break
+        }
+
+    }
+    println("Programa Finalizado")
 }
