3e9e003455f4e7fc30890fd743aa8ff626bcd60f
Nicholas Augusto <nicholas.augusto@cs.cruzeirodosul.edu.br>
2026-08-19 03:58:26 -0300

    aprendizado sobre classes(Construtor, Init)

--- src/main/kotlin/Pessoa.kt
+++ src/main/kotlin/Pessoa.kt
@@ -1,15 +1,50 @@
-class Pessoa(var nome: String, var idade: Int) { //convenção: classe são sempre nomeadas com a inicial em maisculo.
+class Pessoa(var nome: String, val anoNascimento: Int) { //convenção: classe são sempre nomeadas com a inicial em maisculo.
+
+    var doc: String? = null
+
+    // Init - executado quando a classe é instanciada
+    init{
+        if(anoNascimento < 0){
+            throw Exception("Ano nascimento não pode ser negativo")
+        }
+    }
+    init{
+        println("Classe criada")
+    }
+
+
+
+    //secundario
+    constructor(nome: String, anoNascimento: Int, doc: String) : this(nome, anoNascimento) {}
+
     fun acordar() {
 
     }
-    fun falar() {
+    /* fun falar() {
 
-        println("Olá! Me chamo $nome e tenho $idade anos")
-    }
+         println("Olá! Me chamo $nome e tenho $idade anos")
+     }*/
 }
+/*class Pessoa(var nome: String, val anoNascimento: Int) { //convenção: classe são sempre nomeadas com a inicial em maisculo.
+
+    // Init - executado quando a classe é instanciada
+    init{
+        if(anoNascimento < 0){
+            throw Exception("Ano nascimento não pode ser negativo")
+        }
+    }
+    init{
+        println("Classe criada")
+    }
+    fun acordar() {}
+    fun dormir(){}
+
+}*/
+
 
 fun main() {
-    var p: Pessoa = Pessoa("Nicholas", 30)
+    //val p: Pessoa = Pessoa("Nicholas", 30)
+    val p: Pessoa = Pessoa("Pessoa", 1800)
 
-    p.falar()
+
 }
